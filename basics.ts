5e5853d7cecdfd8b8040d219dbd39568bee82601
L1ndseyHerman <lherman21@jcu.edu>
2021-10-11 22:35:03 -0400

    Caught up to vid395: primitive types in TypeScript.

---
+++ basics.ts
@@ -0,0 +1,21 @@
+//  Primitives: number, string, boolean, null, undefined, symbol
+//  More complex types: arrays, objects
+//  Function types, parameters
+
+//  Primitives SHOULD BE LOWERCASE, "string", not "String"!
+
+let age: number = 24;
+
+age = 12;
+
+let userName: string;
+
+userName = "Max";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+let hobbies: null;
+
+//  Can't re-assign hobbies now :(
